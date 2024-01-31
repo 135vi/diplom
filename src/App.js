356cfe941736faec42033c380894a9621eb32b79
Sergey Shabanov <shsv382@gmail.com>
2024-01-31 22:01:30 +0300

    Counter изменяется через setState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Count from './components/Count';
+import { useState } from 'react';
+import { Button } from '@mui/material';
+import { Stack } from '@mui/material';
 
 function App() {
+  let [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+  } 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Count count={count} />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Stack spacing={2} direction="row">
+          <Button variant="contained" onClick={increment}>Прибавить</Button>
+          <Button variant="contained" onClick={decrement}>Отнять</Button>
+        </Stack>
       </header>
     </div>
   );
---
+++ src/components/Count.jsx
@@ -0,0 +1,3 @@
+export default function Count({ count }) {
+    return <h1>{ count }</h1>
+}
